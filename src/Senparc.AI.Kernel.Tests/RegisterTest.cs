c2cd2f6004ab7f9b77d80c46dd853eb4850c6777
苏震巍 <zsu@senparc.com>
2023-04-03 00:17:53 +0800

    完善单元测试覆盖

--- src/Senparc.AI.Kernel.Tests/RegisterTest.cs
+++ src/Senparc.AI.Kernel.Tests/RegisterTest.cs
@@ -13,8 +13,11 @@ namespace Senparc.AI.Kernel.Tests
         public void GlobalRegisterTest() {
             var senparcAiSetting = BaseTest._senparcAiSetting; // Senparc.AI.Kernel.Config.SenparcAiSettings;
             Assert.IsNotNull(senparcAiSetting);
+            Assert.AreEqual(AiPlatform.AzureOpenAI, senparcAiSetting.AiPlatform);
             Assert.AreEqual(true, senparcAiSetting.UseAzureOpenAI);
             Assert.AreEqual("https://xxx", senparcAiSetting.AzureEndpoint);
+            Assert.AreEqual("YourKey", senparcAiSetting.ApiKey);
+            Assert.AreEqual("YourOrgId", senparcAiSetting.OrgaizationId);
         }
     }
 }
