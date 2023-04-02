456aab7f4f35323041268f7d4a55b6680bf5e3c5
苏震巍 <zsu@senparc.com>
2023-04-02 22:43:46 +0800

    添加 Senparc.AI.Kernel 项目

---
+++ src/Senparc.AI.Kernel/Config.cs
@@ -0,0 +1,24 @@
+﻿using Senparc.AI.Kernel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Senparc.AI.Kernel
+{
+    /// <summary>
+    /// Senparc.AI.Kernel 配置
+    /// </summary>
+    public class Config
+    {
+        /// <summary>
+        /// 当前配置
+        /// </summary>
+        public static SenparcAiSettings SenparcAiSettings { get; set; }
+
+        static Config()
+        {
+            //初始化 SenaprcAiSettings
+            SenparcAiSettings = new SenparcAiSettings();
+        }
+    }
+}
---
+++ src/Senparc.AI.Kernel/Entities/IAiContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Senparc.Weixin.AI.Senparc.AI.Kernel.Entities
+{
+
+    public interface IAiContext
+    {
+    }
+}
