7c46e15486aff759770545530898c25839c47668
苏震巍 <zsu@senparc.com>
2023-04-02 19:28:37 +0800

    创建基础类

---
+++ src/Senparc.Weixin.AI.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Senparc.Weixin.AI.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ src/Senparc.Weixin.AI/Register.cs
@@ -0,0 +1,33 @@
+﻿using Senparc.CO2NET.RegisterServices;
+using Senparc.CO2NET;
+using Senparc.Weixin.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Senparc.AI.Kernel;
+
+namespace Senparc.Weixin.AI
+{
+    /// <summary>
+    /// 项目启动注册
+    /// </summary>
+    public static class Register
+    {
+
+        /// <summary>
+        /// 注册
+        /// </summary>
+        /// <param name="registerService"></param>
+        /// <param name="senparcAiSetting"></param>
+        /// <returns></returns>
+        public static IRegisterService UseSenparcAI(this IRegisterService registerService, SenparcAiSettings senparcAiSetting = null)
+        {
+            Senparc.AI.Kernel.Config.SenparcAiSettings
+                = senparcAiSetting ??
+                    Senparc.AI.Kernel.Config.SenparcAiSettings ??
+                        new SenparcAiSettings();
+
+            return registerService;
+        }
+    }
+}
