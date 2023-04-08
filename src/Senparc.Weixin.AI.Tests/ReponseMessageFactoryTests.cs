b4e91804c9e9f06b435a2b50ba52deab2dfb19da
苏震巍 <zsu@senparc.com>
2023-04-08 23:54:56 +0800

    添加单元测试，编写 skill

---
+++ src/Senparc.Weixin.AI.Tests/ReponseMessageFactoryTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Senparc.AI.Interfaces;
+using Senparc.AI.Kernel;
+using Senparc.Weixin.AI;
+using Senparc.Weixin.AI.Tests.BaseSupport;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Senparc.Weixin.AI.Tests
+{
+    [TestClass()]
+    public class ReponseMessageFactoryTests : KernelTestBase
+    {
+        [TestMethod()]
+        public async Task GetResponseMessageTest()
+        {
+            var serviceProvider = BaseTest.serviceProvider;
+            var handler = serviceProvider.GetRequiredService<IAiHandler>()
+                            as SemanticAiHandler;
+
+            var openId = "JeffreySu";
+
+            var factory = new ReponseMessageFactory();
+
+            var text = "I want to know which program language is the best one?";
+            await Console.Out.WriteLineAsync("ASK: "+ text);
+            var responseMessageType = await factory.GetResponseMessageAsync(handler, openId, text);
+            Assert.AreEqual("ResponseMessageText", responseMessageType.Trim());
+            await Console.Out.WriteLineAsync();
+
+            text = "Create a logo picture with black ground, looks like a plan, show it's power.";
+            await Console.Out.WriteLineAsync("ASK: "+ text);
+            responseMessageType = await factory.GetResponseMessageAsync(handler, openId, text);
+            Assert.AreEqual("ResponseMessageImage", responseMessageType.Trim());
+            await Console.Out.WriteLineAsync();
+        }
+    }
+}
---
+++ src/Senparc.Weixin.AI/ReponseMessageFactory.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.SemanticKernel.CoreSkills;
+using Microsoft.SemanticKernel.Orchestration;
+using Senparc.AI;
+using Senparc.AI.Interfaces;
+using Senparc.AI.Kernel.Handlers;
+using Senparc.NeuChar.Entities;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Senparc.Weixin.AI
+{
+    public class ReponseMessageFactory
+    {
+        public async Task<string> GetResponseMessageAsync(IAiHandler aiHandler, string openId, string text)
+        {
+            var skAiHandler = aiHandler as Senparc.AI.Kernel.SemanticAiHandler;
+            var iWantToRun = skAiHandler
+                             .IWantTo()
+                             .ConfigModel(ConfigModel.TextCompletion, openId, "text-davinci-003")
+                             .BuildKernel();
+
+            var planner = iWantToRun.ImportSkill(new PlannerSkill(iWantToRun.Kernel)).skillList;
+            var dir = System.IO.Directory.GetCurrentDirectory();
+            await Console.Out.WriteLineAsync("dir:" + dir);
+
+
+            var skillsDirectory = Senparc.CO2NET.Utilities.ServerUtility.ContentRootMapPath("~/App_Data/skills");/* Path.Combine(dir, "..", "..", "..", "skills");*/
+            await Console.Out.WriteLineAsync("skillsDirectory:" + skillsDirectory);
+            iWantToRun.ImportSkillFromDirectory(skillsDirectory, "ResponseChooseSkill");
+
+            //var ask = "I want to know which program language is the best one?";
+
+            var request = iWantToRun.CreateRequest(text, planner["CreatePlan"]);
+            var originalPlan = await iWantToRun.RunAsync(request);
+
+            var plannResult = originalPlan.Result.Variables.ToPlan().PlanString;
+            await Console.Out.WriteLineAsync("Original plan:");
+            await Console.Out.WriteLineAsync(plannResult);
+
+            var executionResults = originalPlan.Result;
+
+            int step = 1;
+            int maxSteps = 10;
+            string messageType = "UnKnow";
+            while (!executionResults.Variables.ToPlan().IsComplete && step < maxSteps)
+            {
+                var stepRequest = iWantToRun.CreateRequest(executionResults.Variables, false, false, planner["ExecutePlan"]);
+                var results = (await iWantToRun.RunAsync(stepRequest)).Result;
+                if (results.Variables.ToPlan().IsSuccessful)
+                {
+                    Console.WriteLine($"Step {step} - Execution results:\n");
+                    Console.WriteLine(results.Variables.ToPlan().PlanString);
+
+                    if (results.Variables.ToPlan().IsComplete)
+                    {
+                        messageType = results.Variables.ToPlan().Result;
+                        Console.WriteLine($"Step {step} - COMPLETE!");
+                        Console.WriteLine(results.Variables.ToPlan().Result);
+                        break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"Step {step} - Execution failed:");
+                    Console.WriteLine("Error Message:" + results.LastException?.Message);
+                    Console.WriteLine(results.Variables.ToPlan().Result);
+                    break;
+                }
+
+                executionResults = results;
+                step++;
+                Console.WriteLine("");
+            }
+
+            await Console.Out.WriteLineAsync("== plan execute finish ==");
+
+            return messageType;
+        }
+
+    }
+}
