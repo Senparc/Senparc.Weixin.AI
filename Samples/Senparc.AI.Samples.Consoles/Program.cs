035bfaded6cd6d12096b6bda760286f3230f134b
苏震巍 <zsu@senparc.com>
2023-04-17 21:23:19 +0800

    完成 ChatSample

--- Samples/Senparc.AI.Samples.Consoles/Program.cs
+++ Samples/Senparc.AI.Samples.Consoles/Program.cs
@@ -1,1 +1,46 @@
-﻿
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Senparc.AI.Interfaces;
+using Senparc.AI.Kernel;
+using Senparc.AI.Samples.Consoles;
+using Senparc.CO2NET;
+using System.Reflection.Emit;
+
+var configBuilder = new ConfigurationBuilder();
+configBuilder.AddJsonFile("appsettings.json", false, false);
+Console.WriteLine("完成 appsettings.json 添加");
+
+var config = configBuilder.Build();
+Console.WriteLine("完成 ServiceCollection 和 ConfigurationBuilder 初始化");
+
+//更多绑定操作参见：https://docs.microsoft.com/en-us/aspnet/core/fundamentals/configuration/?view=aspnetcore-2.2
+var senparcSetting = new SenparcSetting();
+config.GetSection("SenparcSetting").Bind(senparcSetting);
+
+var senparcAiSetting = new Senparc.AI.SenparcAiSetting();
+config.GetSection("SenparcAiSetting").Bind(senparcAiSetting);
+
+var services = new ServiceCollection();
+services.AddScoped<IAiHandler, SemanticAiHandler>();
+services.AddScoped<ChatSample>();
+
+var serviceProvider = services.BuildServiceProvider();
+
+
+Console.WriteLine("请输入序号，开始对应功能测试：");
+Console.WriteLine("[1] GPT对话机器人");
+//Console.WriteLine("[2] 训练 Embedding 任务");
+var index = Console.ReadLine();
+switch (index)
+{
+    case "1":
+        {
+            //对话机器人 Sample
+            var chatSample = serviceProvider.GetRequiredService<ChatSample>();
+            await chatSample.RunAsync();
+        }
+        break;
+    default:
+        Console.WriteLine("序号错误，请重新开始！");
+        break;
+}
